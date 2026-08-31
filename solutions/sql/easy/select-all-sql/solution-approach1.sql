8a83b5a04739b247e38d6121da2a6191c49bbd8b
ASHISH J GUIN <165133149+AJ-Guin@users.noreply.github.com>
2026-08-31 21:26:06 +0530

    ✅ Select All [sql] — approach1

---
+++ solutions/sql/easy/select-all-sql/solution-approach1.sql
@@ -0,0 +1,18 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/select-all-sql/problem?isFullScreen=true
+-- Problem     Select All
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    sql
+-- Status      Accepted
+-- Submitted   2026-08-31, 09:26 p.m.
+-- ──────────────────────────────────────────────────
+
+
+SELECT * FROM CITY
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+

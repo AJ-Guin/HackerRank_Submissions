c8ca0738aaabfa10c82394a531d95402af96ef54
ASHISH J GUIN <165133149+AJ-Guin@users.noreply.github.com>
2026-08-31 21:26:16 +0530

    ✅ Select By ID [sql] — approach1

---
+++ solutions/sql/easy/select-by-id/solution-approach1.sql
@@ -0,0 +1,18 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
+-- Problem     Select By ID
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    sql
+-- Status      Accepted
+-- Submitted   2026-08-31, 09:26 p.m.
+-- ──────────────────────────────────────────────────
+
+
+SELECT * FROM CITY WHERE ID = '1661'
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+

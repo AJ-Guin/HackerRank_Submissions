cc09c3ca69291b178ddcdbcc8c3f76c05b58c1a5
ASHISH J GUIN <165133149+AJ-Guin@users.noreply.github.com>
2026-08-31 21:26:48 +0530

    ✅ Japanese Cities' Attributes [sql] — approach1

---
+++ solutions/sql/easy/japanese-cities-attributes/solution-approach1.sql
@@ -0,0 +1,18 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?isFullScreen=true
+-- Problem     Japanese Cities' Attributes
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    sql
+-- Status      Accepted
+-- Submitted   2026-08-31, 09:26 p.m.
+-- ──────────────────────────────────────────────────
+
+
+SELECT * FROM CITY WHERE COUNTRYCODE = 'JPN'
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+

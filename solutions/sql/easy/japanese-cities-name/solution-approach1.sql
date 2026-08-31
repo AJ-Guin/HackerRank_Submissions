b8989da48f1a66ffcd8a401548cc3131a915d3b0
ASHISH J GUIN <165133149+AJ-Guin@users.noreply.github.com>
2026-08-31 21:27:52 +0530

    ✅ Japanese Cities' Names [sql] — approach1

---
+++ solutions/sql/easy/japanese-cities-name/solution-approach1.sql
@@ -0,0 +1,18 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/japanese-cities-name/problem?isFullScreen=true
+-- Problem     Japanese Cities' Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    sql
+-- Status      Accepted
+-- Submitted   2026-08-31, 09:27 p.m.
+-- ──────────────────────────────────────────────────
+
+
+SELECT NAME FROM CITY WHERE COUNTRYCODE = 'JPN'
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+

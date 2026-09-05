e0dcf69b27477e216d5b7c24413240997c5c353a
ASHISH J GUIN <165133149+AJ-Guin@users.noreply.github.com>
2026-09-05 13:20:17 +0530

    ✅ Weather Observation Station 3 [sql] — approach1

---
+++ solutions/sql/easy/weather-observation-station-3/solution-approach1.sql
@@ -0,0 +1,23 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/weather-observation-station-3/problem?isFullScreen=true
+-- Problem     Weather Observation Station 3
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    sql
+-- Status      Accepted
+-- Submitted   2026-09-05, 01:20 p.m.
+-- ──────────────────────────────────────────────────
+
+
+
+SELECT DISTINCT CITY FROM STATION WHERE ID % 2 = 0;
+
+
+
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+

eb16495677b01e8f84be2a65fcd7b684def6b23a
ASHISH J GUIN <165133149+AJ-Guin@users.noreply.github.com>
2026-09-05 13:21:52 +0530

    ✅ Weather Observation Station 7 [sql] — approach1

---
+++ solutions/sql/easy/weather-observation-station-7/solution-approach1.sql
@@ -0,0 +1,27 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/weather-observation-station-7/problem?isFullScreen=true
+-- Problem     Weather Observation Station 7
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    sql
+-- Status      Accepted
+-- Submitted   2026-09-05, 01:21 p.m.
+-- ──────────────────────────────────────────────────
+
+
+SELECT DISTINCT 
+CITY
+FROM 
+STATION
+WHERE
+CITY LIKE '%A'
+OR CITY LIKE '%E'
+OR CITY LIKE '%I'
+OR CITY LIKE '%O'
+OR CITY LIKE '%U'
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+

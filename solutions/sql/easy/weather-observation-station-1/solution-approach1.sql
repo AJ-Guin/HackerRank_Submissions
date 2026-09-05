b9d48b7784d63559921686f5658c492addf2eb0a
ASHISH J GUIN <165133149+AJ-Guin@users.noreply.github.com>
2026-09-05 13:20:02 +0530

    ✅ Weather Observation Station 1 [sql] — approach1

---
+++ solutions/sql/easy/weather-observation-station-1/solution-approach1.sql
@@ -0,0 +1,18 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/weather-observation-station-1/problem?isFullScreen=true
+-- Problem     Weather Observation Station 1
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    sql
+-- Status      Accepted
+-- Submitted   2026-09-05, 01:19 p.m.
+-- ──────────────────────────────────────────────────
+
+
+SELECT CITY, STATE FROM STATION;
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
